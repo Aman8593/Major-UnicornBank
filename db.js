d573bb699598726e07fca19e7cc5200697b7bade
Aman8593 <amanwadgaonkar8593@gmail.com>
2024-05-29 18:58:46 +0530

    Second Commit:env

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
-// db.js
 const mysql = require('mysql');
+require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Vd@164399',
-    database: 'Bank' // Updated to use the 'Bank' database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database');
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
 });
 
-module.exports = db;
+module.exports = connection;
